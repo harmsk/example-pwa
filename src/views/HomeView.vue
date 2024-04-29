87c74dfecf8290ed9478bc88a8e4fabf2f4c2fab
Kyle J. Harms <harmsk@users.noreply.github.com>
2024-04-29 15:36:48 +0000

    revert back to working prototype

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,8 +6,7 @@ const key = ref("chicago");
 
 <template>
   <main>
-    <!-- <img :src="`${baseDir}images/${fixtures[key].img}`">
-    <h1>Filename: {{ fixtures[key].img }}</h1> -->
-    <h2>Test</h2>
+    <img :src="`${baseDir}images/${fixtures[key].img}`">
+    <h1>Filename: {{ fixtures[key].img }}</h1>
   </main>
 </template>
